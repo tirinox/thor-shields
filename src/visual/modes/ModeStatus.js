a7179034b7fe070090b23fd7d9c078994e5d4867
Tirinox <developer@tirinox.ru>
2022-08-23 16:56:03 +0300

    TC attractor for status

--- src/visual/modes/ModeStatus.js
+++ src/visual/modes/ModeStatus.js
@@ -4,6 +4,8 @@ import {Attractor} from "@/helpers/physics/Attractor";
 import * as THREE from "three";
 import {Config} from "@/config";
 import _ from "lodash";
+import {AttractorShape} from "@/helpers/physics/AttractorShape";
+import {THORChainLogoShape} from "@/helpers/physics/TCLogoShape";
 
 const ANY_STATUS = '*'
 
@@ -15,9 +17,13 @@ export class ModeStatus extends ModeBase {
         this._sideDistance = 600
 
         const force = Config.Physics.BaseForce
+
+        this.tcAttractor = new AttractorShape(THORChainLogoShape.triangles(0, 100, 1.6), force)
+
         this.attractors = {
             [NodeStatus.Active]: [
-                new Attractor(new THREE.Vector3(0.0, 0, 0), force, 0, 0, 0, this._circleRadius),
+                // new Attractor(new THREE.Vector3(0.0, 0, 0), force, 0, 0, 0, this._circleRadius),
+                this.tcAttractor,
             ],
             [NodeStatus.Standby]: [
                 new Attractor(new THREE.Vector3(-this._sideDistance, 0, 0), force, 0, 0, 0, this._circleRadius * 0.3),
