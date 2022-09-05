aa779020d13684c5e7a8b54eec4bbbb52e64bb44
Tirinox <developer@tirinox.ru>
2022-09-05 11:27:24 +0300

    Fix title transparency

--- src/visual/TitleLabel3D.js
+++ src/visual/TitleLabel3D.js
@@ -19,7 +19,9 @@ export class TitleLabel3D extends THREE.Object3D {
         t.anchorY = 'middle'
         t.text = text
         if (billboard) {
-            t.material = createBillboardMaterial(new MeshBasicMaterial())
+            t.material = createBillboardMaterial(new MeshBasicMaterial({
+                transparent: true,
+            }))
         }
         t.rotateX(THREE.MathUtils.degToRad(rotAngle))
         t.sync()
@@ -53,7 +55,7 @@ export class TitleLabel3D extends THREE.Object3D {
             .start()
 
         new TWEEN.Tween(this.t.material)
-            .to({opacity: 0.0}, expandedDuration)
+            .to({opacity: 0.0}, expandedDuration * 0.5)
             .easing(this.easeType)
             .start().onComplete(() => {
             if (kill) {
